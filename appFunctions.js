d9cac4603fd4e131e13e8c681a466c1552606d6b
vinics <vinics.dr@gmail.com>
2022-08-09 10:37:27 -0300

    refactor: Isolate form submit logic to a separate module

--- appFunctions.js
+++ appFunctions.js
@@ -38,4 +38,34 @@ function downloadCssFile(fileContent) {
   document.body.removeChild(downloadAnchorTag)
 }
 
-export { handleExportToCss, downloadCssFile }
+function handleNewColorEntry(event) {
+  // Avoid submit event from Form element default behavior
+  event.preventDefault()
+
+  chrome.storage.local.get([`chromeExtensionColorPicker`], (result) => {
+    // Create new color definition element
+    const colorEntry = new ColorDefinitionElement(inputColor.value, undefined, counter)
+    counter++
+
+    // Check if the element was empty and enables the Export button
+    if (btnExport.classList.contains('btnDisabled')) {
+      btnExport.classList.remove('btnDisabled')
+    }
+
+    // Append new element on popup view (index.html)
+    container.appendChild(colorEntry.htmlElement)
+
+    // Read current color collection on storage
+    let colorCollectionString = result.chromeExtensionColorPicker
+
+    // Guard in case there is no previous color stored
+    if (!colorCollectionString) colorCollectionString = '[]'
+
+    // Save new color entry on storage
+    const colorCollection = JSON.parse(colorCollectionString)
+    colorCollection.push(colorEntry.getDataObj())
+    chrome.storage.local.set({ chromeExtensionColorPicker: JSON.stringify(colorCollection) })
+  })
+}
+
+export { handleExportToCss, downloadCssFile, handleNewColorEntry }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import ColorDefinitionElement from "./ColorDefinitionElement.js"
 import extensionLoad from "./extensionLoad.js"
 import { form, inputColor, container, btnExport } from './domAssets.js'
-import { handleExportToCss } from './appFunctions.js'
+import { handleExportToCss, handleNewColorEntry } from './appFunctions.js'
 
 
 btnExport.addEventListener('click', handleExportToCss)
@@ -12,32 +12,5 @@ let counter = 1
 extensionLoad()
 
 // Save a new color
-form.addEventListener('submit', (event) => {
-  // Avoid submit event from Form element default behavior
-  event.preventDefault()
+form.addEventListener('submit', handleNewColorEntry)
 
-  chrome.storage.local.get([`chromeExtensionColorPicker`], (result) => {
-    // Create new color definition element
-    const colorEntry = new ColorDefinitionElement(inputColor.value, undefined, counter)
-    counter++
-
-    // Check if the element was empty and enables the Export button
-    if (btnExport.classList.contains('btnDisabled')) {
-      btnExport.classList.remove('btnDisabled')
-    }
-
-    // Append new element on popup view (index.html)
-    container.appendChild(colorEntry.htmlElement)
-
-    // Read current color collection on storage
-    let colorCollectionString = result.chromeExtensionColorPicker
-
-    // Guard in case there is no previous color stored
-    if (!colorCollectionString) colorCollectionString = '[]'
-
-    // Save new color entry on storage
-    const colorCollection = JSON.parse(colorCollectionString)
-    colorCollection.push(colorEntry.getDataObj())
-    chrome.storage.local.set({ chromeExtensionColorPicker: JSON.stringify(colorCollection) })
-  })
-})
